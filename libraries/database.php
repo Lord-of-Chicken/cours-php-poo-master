c437941f936c05c571005213da69a7a8b33c15d1
Lord-of-Chicken <77158261+Lord-of-Chicken@users.noreply.github.com>
2021-06-22 11:42:06 +0200

    Commit 1
    Commit 1

---
+++ libraries/database.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+* retourne ubne connexion à la base de données
+* @return PDO
+*/
+
+function getPdo(): PDO
+{
+    $pdo = new PDO('mysql:host=localhost;dbname=blogpoo;charset=utf8', 'root', '', [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+    return $pdo;
+}
---
+++ libraries/utils.php
@@ -0,0 +1,10 @@
+<?php
+// article('articles/show')
+function render(string $path)
+{
+ob_start();
+require('templates/' . $path . '.html.php');
+$pageContent = ob_get_clean();
+require('templates/layout.html.php');
+
+}
