3cd53e54e2e9672397fcbd4a96cda3fcd88d942a
Lord-of-Chicken <77158261+Lord-of-Chicken@users.noreply.github.com>
2021-06-23 10:28:54 +0200

    Début Class
    Début Class

--- article.php
+++ article.php
@@ -1,22 +1,12 @@
 <?php
-require_once('libraries/database.php');
-require_once('libraries/utils.php');
+require_once 'libraries/database.php';
+require_once 'libraries/utils.php';
+require_once 'libraries/models/Article.php';
+require_once 'libraries/models/Comment.php';
 
-/**
- * CE FICHIER DOIT AFFICHER UN ARTICLE ET SES COMMENTAIRES !
- * 
- * On doit d'abord récupérer le paramètre "id" qui sera présent en GET et vérifier son existence
- * Si on n'a pas de param "id", alors on affiche un message d'erreur !
- * 
- * Sinon, on va se connecter à la base de données, récupérer les commentaires du plus ancien au plus récent (SELECT * FROM comments WHERE article_id = ?)
- * 
- * On va ensuite afficher l'article puis ses commentaires
- */
+$articleModel = new Article();
+$commentModel = new Comment();
 
-/**
- * 1. Récupération du param "id" et vérification de celui-ci
- */
-// On part du principe qu'on ne possède pas de param "id"
 $article_id = null;
 
 // Mais si il y'en a un et que c'est un nombre entier, alors c'est cool
@@ -34,12 +24,12 @@ if (!$article_id) {
  * On va ici utiliser une requête préparée car elle inclue une variable qui provient de l'utilisateur : Ne faites
  * jamais confiance à ce connard d'utilisateur ! :D
  */
-$article = findArticle($article_id);
+$article = $articleModel->find($article_id);
 /**
  * 4. Récupération des commentaires de l'article en question
  * Pareil, toujours une requête préparée pour sécuriser la donnée filée par l'utilisateur (cet enfoiré en puissance !)
  */
-$commentaires = findAllComments($article_id);
+$commentaires = $commentModel->findAllWithArticle($article_id);
 
 /**
  * 5. On affiche 
--- index.php
+++ index.php
@@ -6,13 +6,14 @@
  * On va donc se connecter à la base de données, récupérer les articles du plus récent au plus ancien (SELECT * FROM articles ORDER BY created_at DESC)
  * puis on va boucler dessus pour afficher chacun d'entre eux
  */
-require_once('libraries/database.php');
-require_once('libraries/utils.php');
-
+require_once 'libraries/database.php';
+require_once 'libraries/utils.php';
+require_once 'libraries/models/Article.php';
+$models = new Article();
 /**
  * 2. Récupération des articles
  */
-$articles = findAllArticles();
+$articles = $models->findAll();
 
 
 /**
